1c07c344c57ce75c7b1ec2aa3ab04c64e7d8bba7
phucthang1101 <phucthangvt1101@gmail.com>
2020-01-02 01:27:07 +0700

    Updates

---
+++ precache-manifest.7d60c8ed9e708afb461a04d960e28256.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836f4cda9cc528680348e7fa037520a1",
+    "url": "/phucthang1101.github.io/index.html"
+  },
+  {
+    "revision": "1bdd04349860cad4e0c6",
+    "url": "/phucthang1101.github.io/static/css/2.7190192a.chunk.css"
+  },
+  {
+    "revision": "bf262a8b31a59e0efce2",
+    "url": "/phucthang1101.github.io/static/css/main.39bdf954.chunk.css"
+  },
+  {
+    "revision": "1bdd04349860cad4e0c6",
+    "url": "/phucthang1101.github.io/static/js/2.295d8d5b.chunk.js"
+  },
+  {
+    "revision": "d4929da90be13c8d91e583a6681ff137",
+    "url": "/phucthang1101.github.io/static/js/2.295d8d5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf262a8b31a59e0efce2",
+    "url": "/phucthang1101.github.io/static/js/main.91fc07af.chunk.js"
+  },
+  {
+    "revision": "7d0fdc72222d6472ba3d",
+    "url": "/phucthang1101.github.io/static/js/runtime-main.352f47ee.js"
+  },
+  {
+    "revision": "044d1a04a2d316f296a2a5ec9ae4682a",
+    "url": "/phucthang1101.github.io/static/media/project1.044d1a04.png"
+  },
+  {
+    "revision": "34be6f6b2091fcd7689dbdd38e4a824b",
+    "url": "/phucthang1101.github.io/static/media/project2.34be6f6b.png"
+  },
+  {
+    "revision": "13dfc97a53aeaa3204372d5d0a31813c",
+    "url": "/phucthang1101.github.io/static/media/project3.13dfc97a.png"
+  }
+]);
